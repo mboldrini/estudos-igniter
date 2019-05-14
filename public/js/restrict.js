11c0209a6b6f2fd70908d47afdc760b24fce6c6f
matheusboldrini <matheus.souza.boldrini@hotmail.com>
2019-05-14 19:54:32 -0300

    modal

---
+++ public/js/restrict.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $("#btn_add_course").click(function(){
+        $("#modal_course").modal();
+    });
+
+    $("#btn_add_member").click(function (){
+        $("#modal_member").modal();
+    });
+
+    $("#btn_add_user").click(function (){
+        $("#modal_user").modal();
+    });
+
+})
