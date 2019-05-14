771a4782efd93f113dd69c230dfc5871f840c952
matheusboldrini <matheus.souza.boldrini@hotmail.com>
2019-05-14 17:22:53 -0300

    models da area restrita

--- application/views/restrict.php
+++ application/views/restrict.php
@@ -1,5 +1,6 @@
 <section style="min-height: calc(100vh - 83px);" class="light-bg">
 	<div class="container">
+
 		<div class="row">
 			<div class="col-lg-offset-3 col-lg-6 text-center">
 				<div class="section-title">
@@ -9,7 +10,36 @@
 				</div>
 			</div>
 		</div>
+
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6 text-center">
+				<div class="form-group">
+					
+					<a class="btn btn-link"><i class="fa fa-user"></i></a>
+					<a class="btn btn-link" href="restrict/logoff"><i class="fa fa-sign-out"></i></a>
+
+				</div>
+			</div>
+		</div>
 		
 	</div>
-	<!-- /.container -->
+
+	<div class="container">
+	
+		<ul class="nav nav-tabs">
+			<li class="active"><a href="#tab_courses" role="tab" data-toggle="tab">Cursos</a></li>
+			<li><a href="#tab_team" role="tab" data-toggle="tab">Equipe</a></li>
+			<li><a href="#tab_user" role="tab" data-toggle="tab">Usuários</a></li>
+		</ul>
+		
+		<div class="tab-content">
+			<div id="tab_courses" class="tab-pane active">Conteúdo Contato</div>
+			<div id="tab_team" class="tab-pane">Conteúdo Equipe</div>
+			<div id="tab_user" class="tab-pane">Conteúdo Usuários</div>
+		</div>
+	
+	</div>
+
+
+
 </section>
